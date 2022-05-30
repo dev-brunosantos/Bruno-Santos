97a1561fa5b1992225d5d8c9138a3763af0334de
dev-brunosantos <100520104+dev-brunosantos@users.noreply.github.com>
2022-05-30 14:13:17 -0300

    Atualização para dispositivos com pequenas telas

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// CONFIGURAÇÕES DO MENU PRINCIPAL PARA PEQUENAS TELAS
+const menuPrincipal = document.querySelector('nav');
+
+function abrirMenu() {
+    menuPrincipal.style.display = "flex"
+}
+function fecharMenu() {
+    menuPrincipal.style.display = "none"
+}
+// ___________________________________________________
+
+// CONFIGURAÇÕES DO CONTEÚDO PRINCIPAL PARA PEQUENAS TELAS
+
+let tela1 = document.getElementById('inicio');
+let tela2 = document.getElementById('sobre');
+let tela3 = document.getElementById('projetos');
+let tela4 = document.getElementById('contatos');
+
+function inicio() {
+    tela1.style.display = "flex"
+    tela2.style.display = "none"
+    tela3.style.display = "none"
+    tela4.style.display = "none"
+}
+function sobre() {
+    tela1.style.display = "none"
+    tela2.style.display = "flex"
+    tela3.style.display = "none"
+    tela4.style.display = "none"
+}
+function projetos() {
+    tela1.style.display = "none"
+    tela2.style.display = "none"
+    tela3.style.display = "block"
+    tela4.style.display = "none"
+}
+function contatos() {
+    tela1.style.display = "none"
+    tela2.style.display = "none"
+    tela3.style.display = "none"
+    tela4.style.display = "block"
+}
